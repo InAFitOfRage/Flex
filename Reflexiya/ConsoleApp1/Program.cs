024f76f2352153f7e42cab8a403d22ab470c039e
Dmitry Shakin <dshakin@gmcs.ru>
2021-11-12 13:02:21 +0300

    Totoroooooo

---
+++ Reflexiya/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using webapi1.Controllers;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mass = new List<Type> { 
+                typeof(HomeController)
+                // и т.д
+            };
+
+            foreach(var m in mass)
+            {
+                Assembly a = m.Assembly;
+                var types = a.DefinedTypes.Where(t=> t.Name.ToLower().Contains("controller")).ToList();
+                foreach(var t in types)
+                {
+                    var functions = t.GetMethods().Where(f => f.IsPublic).Where(f1 => f1.CustomAttributes.Any(cAttr => cAttr.AttributeType == typeof(ObsoleteAttribute))).ToList();
+                    functions.ForEach(f =>
+                    {
+                        Console.WriteLine(f.DeclaringType.FullName);
+                        Console.WriteLine(f.Name);
+                    });
+                    
+                }
+                Console.Read();
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Reflexiya/webapi1/Controllers/Home.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webapi1.Controllers
+{
+    [ApiController]
+    [Route("api/Home")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("rand")]
+        public int getRandom()
+        {
+            var rand = new Random();
+            return rand.Next(0, 100);
+        }
+
+        [HttpGet]
+        [Route("name")]
+        [Obsolete]
+        public string getName()
+        {
+            return "U name is LOX";
+        }
+        [HttpGet]
+        [Route("isLox")]
+        public bool LoxAmI(string name)
+        {
+            if (name.ToLower().Contains("lox")) return true;
+            else
+                return true;
+        }
+    }
+}
